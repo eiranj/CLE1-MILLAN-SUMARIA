e35962f84f67b3cbbe5409df1253a2614231ea59
eiranj <jettsumaria@gmail.com>
2023-01-02 11:47:32 +0800

    CLE1-MILLAN-SUMARIA

---
+++ app/src/main/java/com/example/cle1/Bracelet.java
@@ -0,0 +1,14 @@
+package com.example.cle1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Bracelet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bracelet);
+    }
+}
---
+++ app/src/main/java/com/example/cle1/Splash.java
@@ -0,0 +1,29 @@
+package com.example.cle1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
+    }
+}
